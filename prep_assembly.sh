0a78e5949b1b9d8cf27f60416b4e6da5e8e4da7d
Isaure Chauvot de Beauchene <41185922+isaureCdB@users.noreply.github.com>
2023-09-07 14:45:22 +0200

    Update prep_assembly.sh

--- prep_assembly.sh
+++ prep_assembly.sh
@@ -1,12 +1,14 @@
-motif=$1
-frag=$2
+motif=$1 #sequence of the trinucleotide
+frag=$2 #index of the fragment
 
 listr=${motif}r.list
 ligandr=`head -n 1 $listr`
 
 #prepare npy arrays for assembling
-python2 $ATTRACTDIR/dump_coordinates.py $motif.dat $ligandr $motif-preatoms.npy `cat $ssRNATTRACT/data/prepostatoms/$motif.preatoms` --ens 2 $listr
-python2 $ATTRACTDIR/dump_coordinates.py $motif.dat $ligandr $motif-postatoms.npy `cat $ssRNATTRACT/data/prepostatoms/$motif.postatoms` --ens 2 $listr
+if [ ! -f $motif-preatoms.npy ];then
+  python2 $ATTRACTDIR/dump_coordinates.py $motif.dat $ligandr $motif-preatoms.npy `cat $ssRNATTRACT/data/prepostatoms/$motif.preatoms` --ens 2 $listr
+  python2 $ATTRACTDIR/dump_coordinates.py $motif.dat $ligandr $motif-postatoms.npy `cat $ssRNATTRACT/data/prepostatoms/$motif.postatoms` --ens 2 $listr
+fi
 
-ln -s $motif-preatoms.npy $frag-preatoms.npy
-ln -s $motif-postatoms.npy $frag-postatoms.npy
+ln -s $motif-preatoms.npy frag$frag-preatoms.npy
+ln -s $motif-postatoms.npy frag$frag-postatoms.npy
